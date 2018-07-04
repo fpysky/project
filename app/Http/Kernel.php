1c245fa8f960d86cae578407b18214441ad94b8a
fengpengyuan <fengpengyuan@live.com>
2018-07-04 09:54:47 +0800

    111

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,6 +66,8 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'adminAuth' => \App\Http\Middleware\AdminAuth::class
+        'adminAuth' => \App\Http\Middleware\AdminAuth::class,
+        'jwt.auth' => \Tymon\JWTAuth\Middleware\GetUserFromToken::class,
+        'jwt.refresh' => \Tymon\JWTAuth\Middleware\RefreshToken::class,
     ];
 }
